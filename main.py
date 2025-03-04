55824ed24852b57e0a33c1cc83ae23da4feb61b7
Bohdan_Novytskyi <gruvoz.1234@gmail.com>
2025-03-04 20:54:58 +0200

    update else v2

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-first_list = [1, 2, 3, 4, 5, 6, 2]
+first_list = [1, 2, 3, 4, 5, 6, 8]
 
 if not first_list or len(first_list) == 1:
     print(first_list)
 else:
-    result_list = first_list.pop()
-    first_list.insert(0, result_list)
+    first_list.insert(0, first_list.pop())
     print(first_list)
